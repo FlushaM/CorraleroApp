f0c1ce546cd9707221068f7ebc30c1e9348d5720
FlushaM <83796763+FlushaM@users.noreply.github.com>
2025-01-07 21:16:23 -0300

    Update db.js

--- corralero-backend/db.js
+++ corralero-backend/db.js
@@ -1,20 +1,22 @@
 const { Pool } = require('pg');
+require('dotenv').config(); // Carga las variables de entorno desde un archivo .env
 
-// Configuración de la conexión a PostgreSQL
+// Configuración de la conexión a PostgreSQL en Neon.tech
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres', // Reemplaza 'postgres' si usas otro usuario
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'Corralerodb', // Asegúrate de que coincida con tu base de datos
-  password: process.env.DB_PASSWORD || '123', // Si tienes contraseña, añádela aquí
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:vJhEZx6y0Vfm@ep-curly-sun-a43l79d4.us-east-1.aws.neon.tech/neondb?sslmode=require',
+  ssl: {
+    rejectUnauthorized: false, // Necesario para conexiones SSL con Neon.tech
+  },
 });
 
+// Eventos de conexión
 pool.on('connect', () => {
-  console.log('Conexión exitosa a la base de datos');
+  console.log('Conexión exitosa a la base de datos en Neon.tech');
 });
 
 pool.on('error', (err) => {
   console.error('Error en la conexión a la base de datos:', err);
 });
 
+// Exportar el pool para usarlo en otros archivos
 module.exports = pool;
