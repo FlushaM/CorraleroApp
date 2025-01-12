134010cde699a570a094aa7fc365f00f320cda6e
FlushaM <83796763+FlushaM@users.noreply.github.com>
2025-01-12 14:41:08 -0300

    modulos-completos

--- supermercado-app/src/App.js
+++ supermercado-app/src/App.js
@@ -3,14 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import AdminDashboard from './components/AdminDashboard';
 import CarniceriaPage from './modules/Carniceria/CarniceriaPage';
+import VerduleriaPage from './modules/Verduleria/VerduleriaPage';
+import RoticeriaPage from './modules/Roticeria/RoticeriaPage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
+                {/* Ruta principal para Login */}
                 <Route path="/" element={<LoginPage />} />
+                
+                {/* Ruta para el Dashboard del Administrador */}
                 <Route path="/dashboard" element={<AdminDashboard />} />
+                
+                {/* Ruta para Carnicería */}
                 <Route path="/carniceria" element={<CarniceriaPage />} />
+                
+                {/* Ruta para Verdulería */}
+                <Route path="/verduleria" element={<VerduleriaPage />} />
+                
+                {/* Ruta para Roticería */}
+                <Route path="/roticeria" element={<RoticeriaPage />} />
             </Routes>
         </Router>
     );
