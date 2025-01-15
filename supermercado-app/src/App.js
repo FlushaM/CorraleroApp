916d489a2d67c1dfabdd64e1bee30dc0e93fa780
FlushaM <83796763+FlushaM@users.noreply.github.com>
2025-01-14 23:08:43 -0300

    v.02

--- supermercado-app/src/App.js
+++ supermercado-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+import LoginPage from './components/Login/LoginPage';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 import CarniceriaPage from './modules/Carniceria/CarniceriaPage';
 import VerduleriaPage from './modules/Verduleria/VerduleriaPage';
 import RoticeriaPage from './modules/Roticeria/RoticeriaPage';
+
+
 
 const App = () => {
     return (
